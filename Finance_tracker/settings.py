f78178598616582014ae90160e0804c48312be64
biswajit dey <bdey862002@gmail.com>
2025-08-11 20:09:30 +0530

    changes ALLOWED_HOST

--- Finance_tracker/settings.py
+++ Finance_tracker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://finance-tracker-dtwr.onrender.com', 'finance-tracker-dtwr.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
